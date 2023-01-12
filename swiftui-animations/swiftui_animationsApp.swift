37a6ec86d8bcf21cc48c0b96f8ace8fd41e6ffbf
Jose Tone <developer.tony17@gmail.com>
2023-01-12 09:41:27 +0100

    Initial Commit

---
+++ swiftui-animations/swiftui_animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_animationsApp.swift
+//  swiftui-animations
+//
+//  Created by José Tony on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
